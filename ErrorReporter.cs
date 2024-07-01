e38c15c6dd0c3773dbe21b55ed2f344ea0dff77d
John31415 <johnmauris04@gmail.com>
2024-07-01 01:19:01 -0400

    Añade la clase que reporta los errores detectados en el código.
    Implementa una clase ErrorReporter que devuelve información sobre en que lugar del código se encuentra determiando error. Contiene los métodos ReportError y Error.

---
+++ ErrorReporter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+using static System.Math;
+
+namespace Odin
+{
+    internal class ErrorReporter
+    {
+        private static void ReportError(int line, int column, string errorLine, string message)
+        {
+            int startIndex = Max(0, column - 40);
+            int length = Min(errorLine.Length - startIndex, 80);
+            errorLine = "... " + errorLine.Substring(startIndex, length) + " ...";
+            string error = "\nError: " + message + "\n   (" + line + ", " + column + ") | " + errorLine + "\n";
+            column = column - startIndex + line.ToString().Length + column.ToString().Length + 14;
+            while (column > 0)
+            {
+                error += " ";
+                column--;
+            }
+            error += "^-- Here.\n";
+
+            //print error
+            WriteLine(error);
+        }
+
+        public static void Error(int line, int column, string errorLine, string message)
+        {
+            ReportError(line, column, errorLine, message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace Odin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
