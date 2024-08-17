392404dfb5189b88bd3f4b93dc5a20d9f63d9066
John31415 <johnmauris04@gmail.com>
2024-08-16 23:51:48 -0400

    Añade la clases Enviroment para almacenar valores de varialbes.
    La clases tiene un diccionario que relaciona string y object como key and value respectivamente.

---
+++ Environment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin
+{
+    internal class Environment
+    {
+        private Dictionary<string, object> values = new Dictionary<string, object>();
+
+        public object Get(Token name)
+        {
+            if (values.ContainsKey(name._lexeme))
+            {
+                return values[name._lexeme];
+            }
+            ThrowError(name, $"Undefined variable {name._lexeme} .");
+            return null!;
+        }
+
+        public void Define(string name, object value)
+        {
+            if(values.ContainsKey(name)) values[name] = value;
+            else values.Add(name, value);
+        }
+
+        private void ThrowError(Token token, string message)
+        {
+            ErrorReporter.ThrowError(message, token._line, token._position, token._lineBeginning);
+        }
+    }
+}
