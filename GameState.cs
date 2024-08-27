f6d0ffef24b2aa0e0c761c95bfcfc1727ec18ff2
John31415 <johnmauris04@gmail.com>
2024-08-27 01:51:20 -0400

    Añade la clase GameState que recibe el estado del juego y lo devuelve.
    Esta clase permite el intercambio del estado de las cartas en el tablero antes y despues de aplicar el efecto.

---
+++ GameState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin
+{
+    public class GameState
+    {
+        internal long TriggerPlayer { get; set; }
+        internal Lists Board { get; set; }
+        internal Lists Deck { get; set; }
+        internal Lists OtherDeck { get; set; }
+        internal Lists Hand { get; set; }
+        internal Lists OtherHand { get; set; }
+        internal Lists Field { get; set; }
+        internal Lists OtherField { get; set; }
+        internal Lists Graveyard { get; set; }
+        internal Lists OtherGraveyard { get; set; }
+
+        public GameState(long triggerPlayer, Lists board, Lists deck, Lists otherDeck, Lists hand, Lists otherHand, Lists field, Lists otherField, Lists graveyard, Lists otherGraveyard)
+        {
+            TriggerPlayer = triggerPlayer;
+            Board = board;
+            Deck = deck;
+            OtherDeck = otherDeck;
+            Hand = hand;
+            OtherHand = otherHand;
+            Field = field;
+            OtherField = otherField;
+            Graveyard = graveyard;
+            OtherGraveyard = otherGraveyard;
+        }
+    }
+}
