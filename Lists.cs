d93b47b7cdc71da8da8333d0179295fa22a6c43e
John31415 <johnmauris04@gmail.com>
2024-08-27 01:43:27 -0400

    Añade la clases Lists que simula las listas de carta del juego.
    Es instanciada a traves de Field, Board...

---
+++ Lists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odin
+{
+    public class Lists : IClass
+    {
+        public Dictionary<string, object> properties { get; set; }
+        public List<Card> Cards { get; set; }
+
+        public Lists(List<Card> cards)
+        {
+
+            Cards = cards;
+            properties = new Dictionary<string, object>();
+            //properties["Find"] =
+            properties["Push"] = new Push();
+            properties["SendBottom"] = new SendBottom();
+            properties["Pop"] = new Pop();
+            properties["Remove"] = new Remove();
+            properties["Shuffle"] = new Shuffle();
+        }
+    }
+}
